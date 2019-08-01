dcf88df27a1b6cd656eed7197234960e77827dea
Manan Arora <31929934+manan0313@users.noreply.github.com>
2019-08-02 02:54:44 +0530

    Update MainActivity.java
    A simple converter that converts the amount from dollars to rupees. It takes a constant value and multiplies it.

--- app/src/main/java/com/example/converterrupeestodollars/MainActivity.java
+++ app/src/main/java/com/example/converterrupeestodollars/MainActivity.java
@@ -3,12 +3,37 @@ package com.example.converterrupeestodollars;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    EditText etAmount;
+    Button btnConvert;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etAmount = findViewById(R.id.etAmount);
+        btnConvert = findViewById(R.id.btnConvert);
+        btnConvert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String valInDollars = etAmount.getText().toString();
+                Float valInDollarsDouble = Float.parseFloat(valInDollars);
+                Float valInRupeesDouble = (float)(valInDollarsDouble * 69.30);
+                String valInRupees = String.valueOf(valInRupeesDouble);
+                Toast.makeText(MainActivity.this,
+                        valInDollars + "$ is " + valInRupees + "₹",
+                        Toast.LENGTH_LONG).show();
+            }
+
+        });
+
     }
+
 }
